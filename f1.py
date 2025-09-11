82eec10a1a780e14de20157eb607bfeeae7644de
gokul0513 <muthuramanrenuga@gmail.com>
2025-09-11 17:10:55 +0530

    Add files via upload

---
+++ f1.py
@@ -0,0 +1,5 @@
+def add():
+    x=int(input("enter a name"))
+    y=int(input("enter a num"))
+    return x+y
+print("sum:",add())
---
+++ function.py
@@ -0,0 +1,13 @@
+def xyz(name):
+    print("hii:",name)
+xyz("gokul")
+
+def xyz():
+    name=input("enter ur name:")
+    print("hello:",name)
+xyz()
+
+def xnxm():
+    n=input("enter ur name")
+    return n
+print("heyy:",xnxm())
